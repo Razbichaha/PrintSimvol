3c4f40f58dfabd41837bb5657982a44743fc2886
Сергей <seregabur@live.ru>
2022-03-30 23:15:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PrintSimvol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Выберите имя для вывода");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Выберите символ");
+            char userChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            int addingSymvol = 2;
+            int rectangleHeight = 3;
+            Console.Write("\n");
+
+                for(int i=0;i<rectangleHeight;i++)
+                {
+                    
+                    if (i == 0)
+                    {
+                    for(int u=0;u<userName.Length+addingSymvol;u++)
+                    {
+                        Console.Write(userChar);
+                    }
+                    Console.Write("\n");
+                    }
+
+                   if (i == 1) 
+                    {
+                    Console.Write(userChar + userName + userChar);
+                    Console.Write("\n"); 
+                    }
+                    
+                   if (i == 2) 
+                   {
+                    for (int u = 0; u < userName.Length + addingSymvol; u++)
+                    {
+                        Console.Write(userChar);
+                    }
+                    Console.Write("\n");
+                    }
+                    
+                    
+                    
+                }
+
+
+            
+
+
+
+           // Console.WriteLine(userName + "  " + userChar);
+            Console.ReadLine();
+        }
+    }
+}
